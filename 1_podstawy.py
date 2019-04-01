79f12495fa00be2547cd6a31808a8eaee46a8de7
Student <student@swps.edu.pl>
2019-04-01 12:43:39 +0200

    pop

---
+++ 1_podstawy.py
@@ -0,0 +1,35 @@
+print("CDV")
+print(2)
+
+#potęgowanie
+'''
+komentarz blokowy
+'''
+#potęgowanie
+potega = 2 ** 10
+print(potega)
+
+#pobieranie danych z klawiatury
+imie = input("podaj imie: ")
+#konkatenacja +
+nazwisko = input("podaj nazwisko: ")
+print("twoje imie z klawiatury: " + imie + "     " + "twoje imie z klawiatury: " + nazwisko)
+
+
+
+print("Podaj swój wiek: ",end="")
+wiek = input()
+print("Twoj wiek: ",wiek," lat")
+
+nazwisko = "Kowalski"
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+
+dlugosc = len(nazwisko)
+print(dlugosc)
+
+ostatniZnak = nazwisko[len(nazwisko)-1]
+print(ostatniZnak)
+
+ostatniZnak = nazwisko[-1]
+print (ostatniZnak)
